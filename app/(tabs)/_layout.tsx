93ca313f3610625f03d0f8ecb7c70f64738d1e19
Serhii Denysiuk <serhiidenysiuk22@gmail.com>
2025-05-04 19:59:30 +0300

    Completed lab1

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,14 @@
-import { Tabs } from 'expo-router';
+import {Tabs} from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import {Platform} from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
+import {HapticTab} from '@/components/HapticTab';
+import {IconSymbol} from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {Colors} from '@/constants/Colors';
+import {useColorScheme} from '@/hooks/useColorScheme';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -20,26 +22,42 @@ export default function TabLayout() {
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            // Use a transparent background on iOS to show the blur effect
             position: 'absolute',
+            paddingBottom: 0
           },
-          default: {},
+          default: {
+            paddingBottom: 0
+          },
+
         }),
+        tabBarLabelStyle: {
+          padding: 0
+        }
       }}>
       <Tabs.Screen
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: ({color}) => <IconSymbol size={28} name="house.fill" color={color}/>,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Gallery',
+          tabBarIcon: ({color}) => <Icon size={26} name="photo" color={color}/>,
         }}
       />
+
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({color}) => <Icon size={28} name="person" color={color}/>,
+        }}
+      />
+
     </Tabs>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,54 +1,46 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {Image, StyleSheet, View} from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import {ThemedText} from '@/components/ThemedText';
+import {ThemedView} from '@/components/ThemedView';
+
+const newsData = [
+  {
+    title: 'Breaking News',
+    image: "https://picsum.photos/320/220",
+    summary: 'This is a short summary of the breaking news.',
+  },
+  {
+    title: 'Tech Update',
+    image: "https://picsum.photos/321/221",
+    summary: 'Latest advancements in technology you should know.',
+  },
+  {
+    title: 'Sports Highlights',
+    image: "https://picsum.photos/323/223",
+    summary: 'Top moments from recent sports events.',
+  },
+];
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
+    <ParallaxScrollView headerBackgroundColor={{light: '#A1CEDC', dark: '#1D3D47'}} headerImage={<Image
+      source={require('@/assets/images/partial-react-logo.png')}
+      style={styles.reactLogo}
+    />}>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">News</ThemedText>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+      <ThemedView style={styles.newsContainer}>
+        {newsData.map((news, index) => (
+          <View key={index} style={styles.newsItem}>
+            <Image src={news.image} style={styles.newsImage}/>
+            <View>
+              <ThemedText style={styles.textContainer} type="subtitle">{news.title}</ThemedText>
+              <ThemedText style={styles.textContainer}>{news.summary}</ThemedText>
+            </View>
+          </View>
+        ))}
       </ThemedView>
     </ParallaxScrollView>
   );
@@ -56,13 +48,26 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
     alignItems: 'center',
-    gap: 8,
+    marginBottom: 16,
   },
-  stepContainer: {
-    gap: 8,
+  newsContainer: {
+    flexDirection: 'column',
+    gap: 16
+  },
+  newsItem: {
+    backgroundColor: 'rgba(220,220,222,0.65)',
+    borderRadius: 8,
+    flexDirection: "row",
+    padding: 10,
+    gap: 10
+  },
+  newsImage: {
+    height: 80,
+    borderRadius: 8,
     marginBottom: 8,
+    objectFit: 'cover',
+    aspectRatio: 1
   },
   reactLogo: {
     height: 178,
@@ -71,4 +76,8 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
+  textContainer: {
+    flexWrap: "wrap",
+    maxWidth: "92%"
+  }
 });
